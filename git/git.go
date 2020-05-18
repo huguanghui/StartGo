e8e2f38cabf3d17de98d7deae588d5f7d760d430
huguanghui <522146829@qq.com>
2020-05-18 08:25:03 +0800

    添加git模块实现

--- git/git.go
+++ git/git.go
@@ -2,6 +2,9 @@ package git
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/huguanghui/StartGo/cmd"
@@ -19,6 +22,109 @@ func Version() (string, error) {
 }
 
 var cachedDir string
+
+func Dir() (string, error) {
+	if cachedDir != "" {
+		return cachedDir, nil
+	}
+
+	output, err := gitOutput("rev-parse", "q", "--git-dir")
+	if err != nil {
+		return "", fmt.Errorf("Not a git repository (or any of the parent directories): .git")
+	}
+
+	var chdir string
+	for i, flag := range GlobalFlags {
+		if flag == "-C" {
+			dir := GlobalFlags[i+1]
+			if filepath.IsAbs(dir) {
+				chdir = dir
+			} else {
+				chdir = filepath.Join(chdir, dir)
+			}
+		}
+	}
+
+	gitDir := output[0]
+
+	if !filepath.IsAbs(gitDir) {
+		if chdir != "" {
+			gitDir = filepath.Join(chdir, gitDir)
+		}
+
+		gitDir, err = filepath.Abs(gitDir)
+		if err != nil {
+			return "", err
+		}
+
+		gitDir = filepath.Clean(gitDir)
+	}
+
+	cachedDir = gitDir
+	return gitDir, nil
+}
+
+func WorkdirName() (string, error) {
+	output, err := gitOutput("rev-parse", "--show-toplevel")
+	if err == nil {
+		if len(output) > 0 {
+			return output[0], nil
+		} else {
+			return "", fmt.Errorf("unable to determine git working directory")
+		}
+	} else {
+		return "", err
+	}
+}
+
+func HasFile(segments ...string) bool {
+	output, err := gitOutput("rev-parse", "-q", "--git-path", filepath.Join(segments...))
+	if err == nil && output[0] != "--git-path" {
+		if _, err := os.Stat(output[0]); err == nil {
+			return true
+		}
+	}
+
+	dir, err := Dir()
+	if err != nil {
+		return false
+	}
+
+	s := []string{dir}
+	s = append(s, segments...)
+	path := filepath.Join(s...)
+	if _, err := os.Stat(path); err == nil {
+		return true
+	}
+
+	return false
+}
+
+func BranchAtRef(paths ...string) (name string, err error) {
+	dir, err := Dir()
+	if err != nil {
+		return
+	}
+
+	segments := []string{dir}
+	segments = append(segments, paths...)
+	path := filepath.Join(segments...)
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return
+	}
+
+	n := string(b)
+	refPrefix := "ref: "
+	if strings.HasPrefix(n, refPrefix) {
+		name = strings.TrimPrefix(n, refPrefix)
+		name = strings.TrimSpace(name)
+	} else {
+		err = fmt.Errorf("No branch info in %s: %s", path, n)
+	}
+
+	return
+}
 
 func gitOutput(input ...string) (outputs []string, err error) {
 	cmd := gitCmd(input...)
